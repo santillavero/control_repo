8e7255b47e72d99d70a4b4688d4392e8c25a1772
santillavero <36694872+santillavero@users.noreply.github.com>
2018-02-23 09:20:36 +1000

    Update master_server.pp

--- site/role/manifests/master_server.pp
+++ site/role/manifests/master_server.pp
@@ -1,15 +1,4 @@
 class role::master_server {
   include profile::base
   include profile::agent_nodes
-
-  file {'/root/README':
-    ensure  => file,
-    content => "Welcome to ${fqdn} of type ${os.family}\n",
-    owner   => 'root',
-  }
-  file {'/root/INTRO':
-    ensure  => file,
-    content => "Welcome to ${fqdn} of type ${os.family}\n",
-  } 
-  
 }
