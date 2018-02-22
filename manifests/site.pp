a6879e7047d556393418a70367104e647e2c3ff5
santillavero <36694872+santillavero@users.noreply.github.com>
2018-02-23 09:21:51 +1000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,20 +1,23 @@
 node default {
+}
+
+node 'pluto.universe' {
+  include role::master_server
   file {'/root/README':
     ensure  => file,
     content => "Welcome to ${fqdn} of type ${os.family}\n",
     owner   => 'root',
-    }
+  }
   file {'/root/INTRO':
     ensure  => file,
     content => "Welcome to ${fqdn} of type ${os.family}\n",
-    }    
+  }
 }
-node 'pluto.universe' {
-  include role::master_server
-}
+
 node /^web/ {
   include role::app_server
 }
+
 node /^db/ {
   include role::db_server
 }
