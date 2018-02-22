09dff0e7495e4b556ea6886d1baeb7287adbb7db
santillavero <36694872+santillavero@users.noreply.github.com>
2018-02-23 09:13:30 +1000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,7 @@
 node default {
   file {'/root/README':
     ensure  => file,
-    content => 'This is a readme',
+    content => "Welcome to ${fqdn} of type ${os.type}",
     owner   => 'root',
     }
 }
