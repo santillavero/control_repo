e3bb4bbfabaf64aecf58685ec4a027ae825ff2fb
santillavero <36694872+santillavero@users.noreply.github.com>
2018-02-23 14:48:34 +1000

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,6 +2,7 @@ class profile::agent_nodes {
   include dockeragent
   dockeragent::node {'web.puppet.vm':}
   dockeragent::node {'db.puppet.vm':}
+  dockeragent::node {'mine.puppet.vm':}
   host { 'web.puppet.vm':
     ensure => present,
     ip => '172.18.0.2',
@@ -10,8 +11,8 @@ class profile::agent_nodes {
     ensure => present,
     ip => '172.18.0.3',
   }  
-  host { 'adm.puppet.vm':
-    ensure => present,
-    ip => '172.18.0.4',
-  }    
+#  host { 'adm.puppet.vm':
+#    ensure => present,
+#    ip => '172.18.0.4',
+#  }
 }
