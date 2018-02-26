3bd3781ce61c11bb3d27c5cff2c64188520c2f62
santillavero <36694872+santillavero@users.noreply.github.com>
2018-02-26 14:49:33 +1100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,6 +1,10 @@
 class minecraft (
   $url='https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
   $install_dir='/opt/minecraft'
+  $minecraft_world='Universe',
+  $minecraft_port=25565,
+  $minecraft_seed="",
+  $fqdn="Minecraft"
 ){
   file {$install_dir:
     ensure => directory,
@@ -22,10 +26,19 @@ class minecraft (
     content => epp('minecraft/minecraft.service',{
       install_dir => $install_dir
     }),
+  file {"${install_dir}/server.configuration":
+    ensure => file,
+    content => epp('minecraft/server.configuration',{
+      install_dir => $install_dir,
+      minecraft_world => $minecraft_world,
+      minecraft_port => $minecraft_port,
+      minecraft_seed => $minecraft_seed,
+      fqdn => $fqdn
+    }),    
   }
   service {'minecraft':
     ensure => running,
     enable => true,
-    require => [Package['java'],File["${install_dir}/eula.txt"],File['/etc/systemd/system/minecraft.service']],
+    require => [Package['java'],File["${install_dir}/eula.txt"],File['/etc/systemd/system/minecraft.service'],File["${install_dir}/server.configuration"]],
   }
 }
