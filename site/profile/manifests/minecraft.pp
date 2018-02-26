9b55e13caaf7a98404e72222e1cf480ab27cdd8f
santillavero <36694872+santillavero@users.noreply.github.com>
2018-02-26 15:03:08 +1100

    Update minecraft.pp

--- site/profile/manifests/minecraft.pp
+++ site/profile/manifests/minecraft.pp
@@ -1,5 +1,9 @@
 class profile::minecraft {
   class {'minecraft':
     install_dir => '/srv/minecraft',
+    minecraft_world => 'Universe',
+    minecraft_port => 25565,
+    minecraft_seed => '',
+    fqdn=> 'Minecraft'    
   }
 }
